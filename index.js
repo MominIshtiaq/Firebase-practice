81a67e1484933a37cb3e1aa21cc1a2d6d3bbde49
MominIshtiaq <momin989ishtiaq@gmail.com>
2025-11-14 17:33:55 +0500

    added signup and signin functionality using firebase

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 /* === Imports === */
 import { initializeApp } from "firebase/app";
 
+// sendEmailVerification this method will send the user an email to verify themself
+import {
+  // This function will let us create an instance of the Firebase authentication service to use in our application
+  getAuth,
+
+  // This method will help us create the user with traditional email and password.
+  // This function takes three arguments auth, email and password
+  // This auth is the instance of Firebase authentication service we get from getAuth method. like const auth = getAuth(app)
+  createUserWithEmailAndPassword,
+
+  // This method will help us sign/login the user with traditional email and password.
+  // This function takes three arguments auth, email and password
+  // This auth is the instance of Firebase authentication service we get from getAuth method. like const auth = getAuth(app)
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
 // /* === Firebase Setup === */
 const firebaseConfig = {
-  apiKey: "AIzaSyCduBICpjQ7QtusKfjKja34M5LnmFltfDY",
-  authDomain: "practice-firebase-fe397.firebaseapp.com", // Domain for the firebase authentication
-  projectId: "practice-firebase-fe397",
-  storageBucket: "practice-firebase-fe397.firebasestorage.app", // URL for the Firebase storage bucket
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN, // Domain for the firebase authentication
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET, // URL for the Firebase storage bucket
 };
 
 const app = initializeApp(firebaseConfig);
+// By using this auth object, we'll be able to do signing up to user and signing in user
+const auth = getAuth(app);
 
 // app have an options object that will have all the value that we have assigned in the firebaseConfig
 // console.log(app.options.apiKey) => AIzaSyCduBICpjQ7QtusKfjKja34M5LnmFltfDY
@@ -54,11 +72,32 @@ function authSignInWithGoogle() {
 }
 
 function authSignInWithEmail() {
-  console.log("Sign in with email and password");
+  const email = emailInputEl.value;
+  const password = passwordInputEl.value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // const user = userCredential.user;
+      showLoggedInView();
+    })
+    .catch((error) => {
+      console.error("error message while signing user", error.message);
+    });
 }
 
 function authCreateAccountWithEmail() {
-  console.log("Sign up with email and password");
+  const email = emailInputEl.value;
+  const password = passwordInputEl.value;
+
+  // this method is use to create user using traditional email and password
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // const user = userCredential.user;
+      showLoggedInView();
+    })
+    .catch((error) => {
+      console.error("error message while creating user", error.message);
+    });
 }
 
 /* == Functions - UI Functions == */
